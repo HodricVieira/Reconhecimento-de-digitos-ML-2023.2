e99ba79ec24594ef5b7e9f89d6725db91aa97750
Bergson Melo <bergson.melo@academico.ufpb.br>
2024-03-18 21:55:27 -0300

    regressao linear implementada

--- modelos.py
+++ modelos.py
@@ -50,48 +50,27 @@ class RegressaoLogistica():
 ####################################################################################################
 
 class RegressaoLinear():
-    def __init__(self, w = []):
-        self.w = w
-
-# Função que vai atribuir a cada amostra uma probabilidade de ser 1 ou 5 (1 ou -1)
-    def probabilidades(self,w, X):
-        z = np.matmul(w, X) # Esse aqui é o X.T para a operaçao ser possivel. Tambem pode ser feito com np.dot(w, X)
-        probabilidades_amostrais = 1/(1+np.exp(-z))
-        return probabilidades_amostrais
-    
-    # Funçção que vai atribuir a cada amostra uma classe de acordo com sua probabilidade
-    def atribuicao_de_classes(self,probabilidades_amostrais):
-        classes_amostrais_previstas = np.zeros(len(probabilidades_amostrais))
-        classes_amostrais_previstas[np.where(probabilidades_amostrais<0.5)] = -1
-        classes_amostrais_previstas[np.where(probabilidades_amostrais>=0.5)] = 1
-        return classes_amostrais_previstas
-    
-    # Função que vai calcular a acuracia do modelo
-    def calculate_accuracy(self,classes_amostrais_previstas, y):
-        accuracy = (sum(classes_amostrais_previstas==y)/len(y))*100
-        return accuracy
-
-    def fit(self, X, Y):
-        X =  np.array(X)
-        y =  np.array(Y)
-        Xt = X.transpose()
-        XtX = np.dot(Xt, X)
-        inversa = np.linalg.inv(XtX)
-        X_cruz = np.dot(inversa, Xt)
-        w = np.dot(X_cruz, y)
-
-        accuracies = []
-        probabilidades_amostrais = self.probabilidades(w, X)
-        classes_amostrais_previstas = self.atribuicao_de_classes(probabilidades_amostrais)
-        accuracies.append(self.calculate_accuracy(classes_amostrais_previstas, y))
-
-        self.classes_amostrais_previstas = classes_amostrais_previstas
-        self.w = w
-        self.accuracy = accuracies[-1]
-
-        return w, accuracies[-1], classes_amostrais_previstas
-
-    def get_w(self):
+    def fit(self, _X, _y):
+        X = np.array(_X) # X é uma matriz de amostras
+        Y = np.array(_y)
+        XtX = np.dot(X.transpose(), X)
+        XtX_inv = np.linalg.inv(XtX)
+        XtY = np.dot(X.transpose(), Y)
+        self.w = np.dot(XtX_inv, XtY) # w é um vetor unico
+     
+    def atribuicao_de_classes(self, _x):
+        x = np.array(_x)
+        classes_amostrais_previstas = []
+        probabilidades = np.dot(x, self.w)
+        for probabilidade in probabilidades:
+            if probabilidade > 0:
+                classes_amostrais_previstas.append(1)
+            else:
+                classes_amostrais_previstas.append(-1)
+        
+        return np.array(classes_amostrais_previstas)
+     
+    def getW(self):
         return self.w
 
 ####################################################################################################
